557f87560de373f2275dda3cbf16e266f1b29ea2
Sébastien HUSS <sebastien.huss@gmail.com>
2019-06-11 17:55:01 +0200

    1er commit

---
+++ database/db.sql
@@ -0,0 +1,20 @@
+-- create a new database
+CREATE DATABASE demo_node_app;
+
+-- create a user
+create user 'demo'@'%' identified by 'demo';
+grant ALL on demo_node_app.* to demo@'%';
+flush privileges;
+
+
+-- to use database
+use demo_node_app;
+
+-- creating a new table
+CREATE TABLE customer (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  phone VARCHAR(15)
+);
+
---
+++ src/app.js
@@ -0,0 +1,47 @@
+const express = require('express'),
+      path = require('path'),
+      morgan = require('morgan'),
+      mysql = require('mysql'),
+      myConnection = require('express-myconnection'),
+      _ = require('lodash');
+
+const configDefault = {
+    "database": {
+	"port": 3306,
+	"database": "demo_node_app",
+	"host": "localhost",
+	"user": "demo",
+	"password": "demo"
+    }
+}
+const configFile = require('../config.json');
+const config = _.merge(configDefault, configFile);
+
+
+const app = express();
+
+app.set('version', '1.0.0');
+
+// importing routes
+const customerRoutes = require('./routes/customer');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(morgan('dev'));
+app.use(myConnection(mysql, config.database, 'single'));
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use('/', customerRoutes);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// starting the server
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`);
+});
